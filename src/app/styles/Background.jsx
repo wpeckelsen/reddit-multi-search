573425c3d0616fe40b5f0bf5045262a026e586ed
Wessel <wessel@MacBook-Air-tilhrende-Wessel.local>
2024-03-13 21:11:18 +0100

    background decent

--- src/app/styles/Background.jsx
+++ src/app/styles/Background.jsx
@@ -10,7 +10,7 @@ export default function Background() {
                     <div className="dot"></div>
                     <div className="dot"></div>
                     <div className="dot"></div>
-                    <div className="dot"></div>
+                    {/* <div className="dot"></div> */}
                 </div>
             
         </>
