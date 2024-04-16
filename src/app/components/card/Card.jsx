fce06c785b38c68f7331602974073fa82052fd64
Wessel <wessel@MacBook-Air-tilhrende-Wessel.local>
2024-04-16 17:18:55 +0200

    login signup switcher

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./card.scss";
 
-export default function Card({ size, title, subtitle, content }) {
+export default function Card({ size, title, switchTitle, subtitle, content }) {
     return (
         <>
         
             <div className={size}>
             <h2>{title}</h2>            
+            <span>{switchTitle}</span>
                 <p>{subtitle}</p>
                 <div className="error"></div>
                 <div className="content">{content}</div>
